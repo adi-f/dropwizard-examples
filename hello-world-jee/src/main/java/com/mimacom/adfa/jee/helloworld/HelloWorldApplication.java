4cfdae83a9c7b02bc97e4e8ad535246a39d402db
adfa <adrian.fahrer@mimacom.com>
2015-06-01 22:09:08 +0200

    added JEE example

---
+++ hello-world-jee/src/main/java/com/mimacom/adfa/jee/helloworld/HelloWorldApplication.java
@@ -0,0 +1,9 @@
+package com.mimacom.adfa.jee.helloworld;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class HelloWorldApplication extends Application{
+
+}
---
+++ hello-world-jee/src/main/java/com/mimacom/adfa/jee/helloworld/HelloWorldController.java
@@ -0,0 +1,30 @@
+package com.mimacom.adfa.jee.helloworld;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.commons.lang3.StringUtils;
+
+@Path("/")
+public class HelloWorldController {
+
+    private final String greeting;
+    private final String defaultName;
+
+
+    public HelloWorldController(/*String greeting, String defaultName*/) {
+        this.greeting = "Hello test %s!";//greeting;
+        this.defaultName = "JEE"; //defaultName;
+    }
+
+    @GET
+    @Path("say-hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayHello(@QueryParam("name") String name) {
+        name = StringUtils.defaultString(name, defaultName);
+        return String.format(greeting, name);
+    }
+}
