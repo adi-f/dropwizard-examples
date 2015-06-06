757b1f17029f13aae354c1ea1418cb4c46b8954a
adfa <adrian.fahrer@mimacom.com>
2015-06-06 19:52:18 +0200

    arquillian + JAX-RS

--- hello-world-jee/src/test/java/com/mimacom/adfa/jee/helloworld/HelloWorldControllerTest.java
+++ hello-world-jee/src/test/java/com/mimacom/adfa/jee/helloworld/HelloWorldControllerTest.java
@@ -4,9 +4,12 @@ import static org.junit.Assert.*;
 
 import java.net.URL;
 
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.extension.rest.client.ArquillianResteasyResource;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
@@ -26,7 +29,12 @@ public class HelloWorldControllerTest {
     }
 
     @Test
-    public void testSayHello() throws Exception {
-        System.out.println("test");
+    public void testSayHello(@ArquillianResteasyResource WebTarget webTarget) throws Exception {
+        WebTarget path = webTarget.path("say-hello");
+        Invocation.Builder request = path.request();
+        String result = request.get(String.class);
+
+        // String result = webTarget.path("say-hello").request().get(String.class);
+        assertEquals("Hello World!", result);
     }
 }
